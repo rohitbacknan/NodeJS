e0f1a2081bbff94161b5116a16725bb75919ba37
rpuri@luc.edu <rpuri@luc.edu>
2015-09-22 01:23:25 -0500

    first commit

---
+++ myapp/app.js
@@ -0,0 +1,14 @@
+var express    =    require('express');
+var app        =    express();
+
+require('./router/main')(app);
+app.set('views',__dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+var server     =    app.listen(3000,function(){
+    var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+}); 
